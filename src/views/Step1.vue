f9afd446e7b3e84b4a428f440ab5b0a2f100d388
ilia <ilyakut@gmail.com>
2019-02-24 17:03:35 +0300

    img

--- src/views/Step1.vue
+++ src/views/Step1.vue
@@ -2,8 +2,24 @@
   <div>
     <MainPage header="Шаг 1: Расскажи нам о своих навыках" nextRouterLink="step2">
       <div>
-        <b-img src="skillLevel.PNG" fluid alt="Responsive image" />
+        <img src="../assets/level.png" height="115" width="90%"/>
       </div>
+      <b-container class="pb-4 pl-0">
+        <b-row class="justify-content-center">
+          <b-col md="3" class="p-0">
+            <h6>Не слышал об этом</h6>
+          </b-col>
+          <b-col md="3" class="p-0">
+            <h6>Имею представление</h6>
+          </b-col>
+          <b-col md="3" class="p-0">
+            <h6>Умею применять</h6>
+          </b-col>
+          <b-col md="3" class="p-0">
+            <h6>Отлично разбираюсь</h6>
+          </b-col>
+        </b-row>
+      </b-container>
       <SkillsComponent/>
     </MainPage>
   </div>
